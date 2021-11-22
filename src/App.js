bf490cd7d326639df0f2e3e1593181c97fdddbaf
Muhammed Fatih Ozdil <muhammedfatihozdil@gmail.com>
2021-11-22 20:43:21 +0300

    sign in page added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import {Redirect, Route, Switch, withRouter } from "react-router-dom";
+import "./App.css";
+import { Redirect, Route, Routes, withRouter } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
+import SignInPage from "./containers/SignInPage/SignInPage";
 function App() {
-    let routes;
-/*
-    routes = (
-        <Switch>
-            <Route path="/" component= 
-            */
-  return (
-    <div className="App">
-      <Layout>
+  let routes;
 
-      </Layout>
+  routes = (
+    <Routes>
+      <Route path="/signin" exact element={<SignInPage />} />
+    </Routes>
+  );
+
+  return (
+    <div className="App" style={{ backgroundColor: "#d9d9d9" }}>
+      <Layout>{routes}</Layout>
     </div>
   );
 }
--- src/containers/SignInPage/SignInPage.js
+++ src/containers/SignInPage/SignInPage.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import classes from './SignInPage.module.scss';
+import React from "react";
+import classes from "./SignInPage.module.scss";
 
-const SignInPage = props => {
+const SignInPage = (props) => {
+  return (
+    <div className={classes.SignInPage}>
+      <h3> Yönetici Girişi </h3>
+      <div className={classes.signInBox}>
+        <div className={classes.input}>
+          <label for="email">EPosta</label>
+          <input type="email" id="email" />
+        </div>
 
-
-	return(
-		<div className={classes.>
-		
-		</div>
-	);
-
-
-}
-
+        <div className={classes.input}>
+          <label for="password">Şifre</label>
+          <input type="password" id="password" />
+        </div>
+        <p className={classes.forgatPassword}>Şifremi Unuttum!</p>
+        <button className={classes.button}>Giriş Yap</button>
+      </div>
+    </div>
+  );
+};
+export default SignInPage;
