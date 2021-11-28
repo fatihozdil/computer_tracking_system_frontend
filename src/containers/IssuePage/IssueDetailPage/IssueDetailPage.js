68e063fc9a3f676a73b1854e6d0adfdaaf46a8f9
Muhammed Fatih Ozdil <muhammedfatihozdil@gmail.com>
2021-11-28 23:13:38 +0300

    add solver message component to the issue detail page

--- src/containers/IssuePage/IssueDetailPage/IssueDetailPage.js
+++ src/containers/IssuePage/IssueDetailPage/IssueDetailPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import ComputerProperties from "../../../components/Computer/ComputerProperties/ComputerProperties";
 import IssueTable from "../../../components/Issue/IssueTable/IssueTable";
 import SenderTable from "../../../components/Issue/SenderTable/SenderTable";
+import SolverMessageInput from "../../../components/Issue/SolverMessageInput/SolverMessageInput";
 import SolverTable from "../../../components/Issue/SolverTable/SolverTable";
 import DropdownTable from "../../../components/UI/DropdownTable/DropdownTable";
 import classes from "./IssueDetailPage.module.scss";
@@ -23,6 +24,8 @@ const IssueDetailPage = (props) => {
         <DropdownTable title="Problemi Çözen Bilgileri">
           <SolverTable />
         </DropdownTable>
+
+        <SolverMessageInput />
 
         <h3>Bu Bilgisayara Ait Bildirilmiş Diğer Sorunlar</h3>
       </div>
